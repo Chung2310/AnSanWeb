0e733308fece9de7ab4f917feb3d4118fb66cd18
iGen Technology <igen.work99@gmail.com>
2026-01-05 01:41:13 +0000

    Try fixing this error: `Runtime Error: Functions cannot be passed direct

--- src/components/homepage/hero-slider.tsx
+++ src/components/homepage/hero-slider.tsx
@@ -1,6 +1,8 @@
+'use client';
+
 import Image from 'next/image';
 import Link from 'next/link';
-import Autoplay from "embla-carousel-autoplay";
+import Autoplay from 'embla-carousel-autoplay';
 
 import {
   Carousel,
