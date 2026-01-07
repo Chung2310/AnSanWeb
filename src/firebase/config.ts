52739f9f9e0ecb6c5449812fdff33b426cdecdbe
iGen Technology <igen.work99@gmail.com>
2026-01-07 06:57:38 +0000

    fix errors:## Error Type Console FirebaseError
    ## Error Message
    Firebas

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-8476793219-f7142",
   "appId": "1:685654143214:web:83004cfe03155185f903ff",
-  "storageBucket": "studio-8476793219-f7142.appspot.com",
+  "storageBucket": "studio-8476793219-f7142.firebasestorage.app",
   "apiKey": "AIzaSyBHTu9qBwPj_hUJd1A30mNbUmb_Dl2hvHQ",
   "authDomain": "studio-8476793219-f7142.firebaseapp.com",
   "measurementId": "",
