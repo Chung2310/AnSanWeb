f93e94529e1af0f8c97470d2e0822dcd38b882a2
iGen Technology <igen.work99@gmail.com>
2026-01-07 12:01:27 +0000

    sao vẫn lỗi?

--- src/components/admin/products/product-form.tsx
+++ src/components/admin/products/product-form.tsx
@@ -120,7 +120,7 @@ export default function ProductForm({ initialData }: ProductFormProps) {
   const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const name = e.target.value;
     form.setValue('nameVN', name);
-    form.setValue('slug', slugify(name, { lower: true, strict: true }));
+    form.setValue('slug', slugify(name, { lower: true, strict: true, remove: /[*+~.()'"!:@]/g }));
   };
   
   const handleImageUrlChange = (e: React.ChangeEvent<HTMLInputElement>) => {
