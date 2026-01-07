ab51f49576967944be9c858f471f50fcf4a579e6
iGen Technology <igen.work99@gmail.com>
2026-01-07 03:00:07 +0000

    menu bên trái hiển thị hẳn ra có cả tên luôn

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -33,7 +33,7 @@ export default function AdminLayout({
   return (
     <div className="flex min-h-screen w-full flex-col bg-muted/40">
       <SidebarNav />
-      <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
+      <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-60">
         <AdminHeader />
         <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
           {children}
