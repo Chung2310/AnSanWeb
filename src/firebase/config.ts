170e391e9ec0b89006ed1cdb86229d5b440ab2ac
iGen Technology <igen.work99@gmail.com>
2026-01-08 02:15:49 +0000

    kiểm tra lại tại sao trang chi tiết sản phẩm của tất cả sản phẩm đều lỗi

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,7 +3,7 @@ export const firebaseConfig = {
   "appId": "1:685654143214:web:83004cfe03155185f903ff",
   "apiKey": "AIzaSyBHTu9qBwPj_hUJd1A30mNbUmb_Dl2hvHQ",
   "authDomain": "studio-8476793219-f7142.firebaseapp.com",
-  "storageBucket": "studio-8476793219-f7142.appspot.com",
+  "storageBucket": "studio-8476793219-f7142.firebasestorage.app",
   "measurementId": "",
   "messagingSenderId": "685654143214"
 };
