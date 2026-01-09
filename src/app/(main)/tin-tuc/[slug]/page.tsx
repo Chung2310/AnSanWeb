83edf34150bac69d6cdc488b387aca78cdd9a563
iGen Technology <igen.work99@gmail.com>
2026-01-09 03:45:06 +0000

    TẠI SAO VẪN KHÔNG CÓ CHI TIẾT BÀI VIẾT?

--- src/app/(main)/tin-tuc/[slug]/page.tsx
+++ src/app/(main)/tin-tuc/[slug]/page.tsx
@@ -2,7 +2,6 @@
 
 import { notFound, useParams } from "next/navigation";
 import Image from "next/image";
-import Link from "next/link";
 import { User, Calendar } from "lucide-react";
 import PostSidebar from "@/components/post-sidebar";
 import type { BlogPost } from "@/lib/types";
