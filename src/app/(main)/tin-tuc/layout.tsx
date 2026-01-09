8fac3791ced6584c69dc36c2d11098f06b7ba40e
iGen Technology <igen.work99@gmail.com>
2026-01-09 03:34:31 +0000

    TÔI TẠO BÀI VIẾT MỚI RỒI VẪN BỊ

--- src/app/(main)/tin-tuc/layout.tsx
+++
@@ -1,6 +0,0 @@
-'use client';
-import BlogListing from '@/components/blog-listing';
-
-export default function BlogLayout({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
-}
