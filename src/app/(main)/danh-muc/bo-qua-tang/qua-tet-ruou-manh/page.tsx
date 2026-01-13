096d9e8a00abc2053f122183ef737afe9392c58a
iGen Technology <igen.work99@gmail.com>
2026-01-13 02:44:51 +0000

    không phải gift-set-wine mà là: qua-tet-ruou-vang; không phải gift-set-s

--- src/app/(main)/danh-muc/bo-qua-tang/qua-tet-ruou-manh/page.tsx
+++ src/app/(main)/danh-muc/bo-qua-tang/qua-tet-ruou-manh/page.tsx
@@ -12,7 +12,7 @@ export default function ProductsPage() {
   
   const giftSetProducts = useMemo(() => {
     if (!products || !categories) return [];
-    const giftSetCategory = categories.find(c => c.slug === 'gift-set-spirits');
+    const giftSetCategory = categories.find(c => c.slug === 'qua-tet-ruou-manh');
     if (!giftSetCategory) return [];
     return products.filter(wine => wine.tags?.includes(giftSetCategory.id));
   }, [products, categories]);
