e689b7923d7aa651f42878bedce8adb35a3c74f2
iGen Technology <igen.work99@gmail.com>
2026-01-20 10:12:06 +0000

    sửa background thành màu trắng ấm (_for element <FamousBrands>_)

--- src/components/homepage/famous-brands.tsx
+++ src/components/homepage/famous-brands.tsx
@@ -71,9 +71,10 @@ export default function FamousBrands() {
         initial="hidden"
         animate={mainControls}
         transition={{ duration: 1, delay: 0.3 }}
-        className="py-16 bg-black">
+        className="py-16"
+        style={{ backgroundColor: '#fdfaf5' }}>
       <div className="container">
-        <h2 className="text-center text-3xl font-bold tracking-wider uppercase text-white">
+        <h2 className="text-center text-3xl font-bold tracking-wider uppercase" style={{color: '#5a5a5a'}}>
           Những Thương Hiệu Nổi Tiếng
         </h2>
         <Carousel
@@ -114,7 +115,7 @@ export default function FamousBrands() {
                     onClick={() => api?.scrollTo(index)}
                     className={cn(
                         "w-2 h-2 rounded-full",
-                        current === index ? "bg-white" : "bg-gray-600"
+                        current === index ? "bg-stone-800" : "bg-stone-400"
                     )}
                     aria-label={`Go to slide ${index + 1}`}
                 />
