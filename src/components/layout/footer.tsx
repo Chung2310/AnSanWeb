6137ca0a74466791173fb84d3415f32d1398b0a1
iGen Technology <igen.work99@gmail.com>
2026-01-21 06:51:21 +0000

    êu cầu: Bản đồ phải hiển thị dạng responsive (co giãn tốt trên cả điện t

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -94,7 +94,7 @@ export default function Footer() {
                     <iframe
                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3725.176426189993!2d105.9610764153835!3d21.11884318599496!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x313507d363d71ddb%3A0x7a29b7c779b993e1!2zUsaw4bujdSBWYW5nIEFuIFNhbg!5e0!3m2!1sen!2s"
                         width="100%"
-                        height="300"
+                        height="450"
                         style={{ border: 0 }}
                         allowFullScreen={false}
                         loading="lazy"
