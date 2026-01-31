e4aa5888cebd644112caa9d4e3d6b841fc079ec3
iGen Technology <igen.work99@gmail.com>
2026-01-31 11:45:04 +0000

    các image hãy chuyển ra giữa (_for element div_)

--- src/components/product-listing.tsx
+++ src/components/product-listing.tsx
@@ -194,7 +194,7 @@ function ProductListingContent({ initialProducts, title, bannerData, itemsPerPag
       </div>
       {categoryDescription && (
         <div className="container py-20">
-            <div className="mx-auto border-t pt-10 text-gray-700 leading-relaxed prose prose-lg max-w-none prose-img:mx-auto prose-img:block">
+            <div className="mx-auto border-t pt-10 text-gray-700 leading-relaxed prose prose-lg max-w-none">
                 <div dangerouslySetInnerHTML={{ __html: categoryDescription }} />
             </div>
         </div>
