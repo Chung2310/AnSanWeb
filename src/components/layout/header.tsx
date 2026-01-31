1a96da8a426eff504a402ac41b7bf2b5e0ecebb8
iGen Technology <igen.work99@gmail.com>
2026-01-31 11:10:58 +0000

    link lại whisky dụa trên trang admin (_for element div_)

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -69,7 +69,7 @@ const staticNavLinks: NavLinkData[] = [
         megaMenuColumns: [
             {
                 title: 'Theo loại rượu',
-                items: spiritsMegaMenuData.theoLoai.map(item => ({ href: `/danh-muc/${item.slug}`, label: item.label }))
+                items: spiritsMegaMenuData.theoLoai.map(item => ({ href: `/danh-muc/ruou-manh/${item.slug}`, label: item.label }))
             },
             {
                 title: 'Thương hiệu',
@@ -77,10 +77,7 @@ const staticNavLinks: NavLinkData[] = [
             },
             {
                 title: 'Quà tặng',
-                items: [{
-                    href: '/danh-muc/qua-tet-ruou-manh',
-                    label: 'Quà Tết Rượu Mạnh'
-                }]
+                items: spiritsMegaMenuData.quaTang.map(item => ({ href: `/danh-muc/bo-qua-tang/${item.slug}`, label: item.label }))
             }
         ]
     },
@@ -109,7 +106,7 @@ const staticNavLinks: NavLinkData[] = [
             {
                 title: 'Quà tặng',
                 items: giftSetMegaMenuData.quaTang.map(item => ({
-                    href: `/danh-muc/${item.slug}`,
+                    href: `/danh-muc/bo-qua-tang/${item.slug}`,
                     label: item.label
                 })),
             }
