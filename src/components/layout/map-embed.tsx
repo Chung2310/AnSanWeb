5225a2d3f0f9374d55a2751a58fc50f2292481fe
iGen Technology <igen.work99@gmail.com>
2026-02-02 09:43:15 +0000

    bản đồ không hiển thị (_for element div_)

--- src/components/layout/map-embed.tsx
+++ src/components/layout/map-embed.tsx
@@ -1,19 +1,6 @@
 'use client';
 
-import { useState, useEffect } from 'react';
-
 export default function MapEmbed() {
-    const [isMounted, setIsMounted] = useState(false);
-
-    useEffect(() => {
-        setIsMounted(true);
-    }, []);
-
-    if (!isMounted) {
-        // Render a placeholder or nothing on the server and during initial client render
-        return <div className="w-full h-[450px] bg-muted animate-pulse" />;
-    }
-
     return (
         <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!2d105.9610764153835!3d21.11884318599496!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x313507d363d71ddb%3A0x7a29b7c779b993e1!2zUsaw4bujdSBWYW5nIEFuIFNhbg!5e0!3m2!1sen!2s"
@@ -21,6 +8,7 @@ export default function MapEmbed() {
             height="450"
             style={{ border: 0 }}
             allowFullScreen={true}
+            loading="lazy"
             referrerPolicy="no-referrer-when-downgrade"
         ></iframe>
     );
