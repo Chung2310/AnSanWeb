295ef6479b8882f2747a7fcfb59d915e0ea57eec
iGen Technology <igen.work99@gmail.com>
2026-02-02 09:50:58 +0000

    tại sao lỗi (_for element div_)

--- src/components/layout/map-embed.tsx
+++ src/components/layout/map-embed.tsx
@@ -3,7 +3,7 @@
 export default function MapEmbed() {
     return (
         <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!2d105.9610764153835!3d21.11884318599496!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x313507d363d71ddb%3A0x7a29b7c779b993e1!2zUsaw4bujdSBWYW5nIEFuIFNhbg!5e0!3m2!1sen!2s"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3721.503380421711!2d105.95849617599663!3d21.11884318497626!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x313507d363d71ddb%3A0x7a29b7c779b993e1!2zUsaw4bujdSBWYW5nIEFuIFNhbg!5e0!3m2!1sen!2s!4v1688888888888!5m2!1sen!2s"
             width="100%"
             height="450"
             style={{ border: 0 }}
