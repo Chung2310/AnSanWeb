cac0c077e1b30dac143f429af126032e8c8aa6b4
iGen Technology <igen.work99@gmail.com>
2026-02-03 07:37:54 +0000

    Try fixing this error: `Recoverable Error: Hydration failed because the

--- src/lib/mega-menu-data.ts
+++ src/lib/mega-menu-data.ts
@@ -1,39 +1,38 @@
 export const wineMegaMenuData = {
     theoLoai: [
-        { label: "Rượu vang đỏ", slug: "ruou-vang-do", category_id: "ruou-vang-do" },
-        { label: "Rượu vang trắng", slug: "ruou-vang-trang", category_id: "ruou-vang-trang" },
-        { label: "Rượu vang sủi", slug: "ruou-vang-sui", category_id: "ruou-vang-sui" },
         { label: "Champagne", slug: "champagne", category_id: "champagne" },
         { label: "Rượu vang 0 độ", slug: "ruou-vang-0-do", category_id: "ruou-vang-0-do" },
+        { label: "Rượu vang đỏ", slug: "ruou-vang-do", category_id: "ruou-vang-do" },
+        { label: "Rượu vang sủi", slug: "ruou-vang-sui", category_id: "ruou-vang-sui" },
+        { label: "Rượu vang trắng", slug: "ruou-vang-trang", category_id: "ruou-vang-trang" },
     ],
     theoQuocGia: [
-        { label: "Vang Pháp", slug: "vang-phap", category_id: "phap" },
-        { label: "Vang Úc", slug: "vang-uc", category_id: "uc" },
-        { label: "Vang Ý", slug: "vang-y", category_id: "y" },
-        { label: "Vang Tây Ban Nha", slug: "vang-tay-ban-nha", category_id: "tay-ban-nha" },
         { label: "Vang Đức", slug: "vang-duc", category_id: "duc" },
         { label: "Vang Nga", slug: "vang-nga", category_id: "nga" },
+        { label: "Vang Pháp", slug: "vang-phap", category_id: "phap" },
+        { label: "Vang Tây Ban Nha", slug: "vang-tay-ban-nha", category_id: "tay-ban-nha" },
+        { label: "Vang Úc", slug: "vang-uc", category_id: "uc" },
+        { label: "Vang Ý", slug: "vang-y", category_id: "y" },
     ],
     theoVung: [
-        { label: "Saint-Émilion", slug: "saint-emilion", category_id: "saint-emilion" },
-        { label: "Pomerol", slug: "pomerol", category_id: "pomerol" },
-        { label: "Languedoc", slug: "languedoc", category_id: "languedoc" },
-        { label: "Puglia", slug: "puglia", category_id: "puglia" },
-        { label: "Veneto", slug: "veneto", category_id: "veneto" },
         { label: "Abruzzo", slug: "abruzzo", category_id: "abruzzo" },
         { label: "Crecchio", slug: "crecchio", category_id: "crecchio" },
-        { label: "Tuscany", slug: "toscana", category_id: "toscana" },
+        { label: "Languedoc", slug: "languedoc", category_id: "languedoc" },
+        { label: "Piemonte", slug: "piemonte", category_id: "piemonte"},
+        { label: "Pomerol", slug: "pomerol", category_id: "pomerol" },
+        { label: "Puglia", slug: "puglia", category_id: "puglia" },
+        { label: "Saint-Émilion", slug: "saint-emilion", category_id: "saint-emilion" },
         { label: "Salento", slug: "salento", category_id: "salento" },
         { label: "Sicily", slug: "sicilia", category_id: "sicilia" },
-        { label: "Piemonte", slug: "piemonte", category_id: "piemonte"}
+        { label: "Tuscany", slug: "toscana", category_id: "toscana" },
+        { label: "Veneto", slug: "veneto", category_id: "veneto" },
     ],
     theoGiongNho: [
         { label: "Cabernet Sauvignon", slug: "cabernet-sauvignon", category_id: "cabernet-sauvignon" },
-        { label: "Merlot", slug: "merlot", category_id: "merlot" },
-        { label: "Shiraz", slug: "shiraz", category_id: "shiraz" },
         { label: "Chardonnay", slug: "chardonnay", category_id: "chardonnay" },
         { label: "Garnacha", slug: "garnacha", category_id: "garnacha" },
         { label: "Malvasia", slug: "malvasia", category_id: "malvasia" },
+        { label: "Merlot", slug: "merlot", category_id: "merlot" },
         { label: "Montepulciano", slug: "montepulciano", category_id: "montepulciano" },
         { label: "Moscato", slug: "moscato", category_id: "moscato" },
         { label: "Negroamaro", slug: "negroamaro", category_id: "negroamaro" },
@@ -42,6 +41,7 @@ export const wineMegaMenuData = {
         { label: "Primitivo", slug: "primitivo", category_id: "primitivo" },
         { label: "Riesling", slug: "riesling", category_id: "riesling" },
         { label: "Sangiovese", slug: "sangiovese", category_id: "sangiovese" },
+        { label: "Shiraz", slug: "shiraz", category_id: "shiraz" },
         { label: "Tempranillo", slug: "tempranillo", category_id: "tempranillo" },
         { label: "Zinfandel", slug: "zinfandel", category_id: "zinfandel" },
     ],
@@ -53,9 +53,9 @@ export const spiritsMegaMenuData = {
     ],
     thuongHieu: [
         { label: "Ballantine's Finest", slug: 'ballantines-finest', category_id: 'ballantines' },
+        { label: 'Chivas', slug: 'chivas', category_id: 'chivas' },
         { label: 'John Walker', slug: 'john-walker', category_id: 'john-walker' },
         { label: 'Mortlach', slug: 'mortlach', category_id: 'mortlach' },
-        { label: 'Chivas', slug: 'chivas', category_id: 'chivas' },
         { label: 'Royal Salute', slug: 'royal-salute', category_id: 'royal-salute' },
         { label: 'The Singleton', slug: 'the-singleton', category_id: 'singleton' },
     ],
@@ -66,14 +66,14 @@ export const spiritsMegaMenuData = {
 
 export const glasswareMegaMenuData = {
     lyPhaLeRiedel: [
+        { label: "Ly Champagne", slug: "ly-champagne", category_id: "ly-champagne" },
         { label: "Ly Vang Đỏ", slug: "ly-vang-do", category_id: "ly-vang-do" },
         { label: "Ly Vang Trắng", slug: "ly-vang-trang", category_id: "ly-vang-trang" },
-        { label: "Ly Champagne", slug: "ly-champagne", category_id: "ly-champagne" },
         { label: "Ly thủy tinh rẻ", slug: "ly-thuy-tinh-re", category_id: "ly-thuy-tinh-re" },
     ],
     lyWhisky: [
-        { label: "Ly Whisky", slug: "ly-whisky", category_id: "ly-whisky" },
         { label: "Cốc Whisky", slug: "coc-whisky", category_id: "coc-whisky" },
+        { label: "Ly Whisky", slug: "ly-whisky", category_id: "ly-whisky" },
     ],
     khac: [
       { label: "Decanter/Bình Thở", slug: "decanter-binh-tho", category_id: "decanter-binh-tho" },
@@ -83,7 +83,7 @@ export const glasswareMegaMenuData = {
 export const giftSetMegaMenuData = {
     quaTang: [
         { label: "Quà Tết An San", slug: "qua-tet-an-san", category_id: "qua-tet-an-san" },
-        { label: "Quà Tết Rượu Vang", slug: "qua-tet-ruou-vang", category_id: "qua-tet-ruou-vang" },
         { label: "Quà Tết Rượu Mạnh", slug: "qua-tet-ruou-manh", category_id: "qua-tet-ruou-manh" },
+        { label: "Quà Tết Rượu Vang", slug: "qua-tet-ruou-vang", category_id: "qua-tet-ruou-vang" },
     ]
 };
