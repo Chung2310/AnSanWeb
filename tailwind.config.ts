5388708464616e7cabafb880b38bd976c435c394
iGen Technology <igen.work99@gmail.com>
2026-02-05 07:24:01 +0000

    hiện tại tôi muốn những sản phầm được giảm giá thì sẽ có phần chữ "Giá đ

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -95,10 +95,15 @@ export default {
             height: '0',
           },
         },
+        'flash': {
+          '0%, 100%': { opacity: '1' },
+          '50%': { opacity: '0.6' },
+        }
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
+        'flash': 'flash 1.5s infinite',
       },
       typography: (theme: any) => ({
         lg: {
