1da355174c03280684758f3b4e1630bc5f6fb0ae
iGen Technology <igen.work99@gmail.com>
2026-02-07 08:56:10 +0000

    hãy cho zone 'Khách hàng yêu thích' ;ên trên zone 'GIÁ TỐT SIÊU ƯU ĐÃI'

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -17,9 +17,9 @@ export default function HomePage() {
       <HeroSection />
       <FamousBrands />
       <DrinkCategoryShowcase />
+      <BestChoiceSection />
       <WhiskyRegionShowcase />
       <InfluenceSection />
-      <BestChoiceSection />
       <PriceCategoryShowcase />
       <GiftSetsSection />
       <Testimonials />
